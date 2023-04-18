8ca490cd1921ea21f7bf6b092e9ec83534b6c147
ireneguerrero <irene2g222@gmail.com>
2023-04-18 19:42:22 +0200

    Update flex2.js

--- flex/flex2.js
+++ flex/flex2.js
@@ -1,4 +1,9 @@
-let cuadro = prompt("Diga qué cuadro quiere saludar (1 - 5)", "");
-cuadro = parseInt(cuadro);
+'use strict'; //nos ayuda a no dejar pasar ningún error
 
-estecuadro = document.getElementById(cuadro);
+//pedimos el número de cuadro donde escribir HOLA
+var cuadro = prompt("Diga qué cuadro quiere saludar (1 - 5)", "");
+cuadro = parseInt(cuadro);
+//cuadro ahora será una variable numérica, supuestamente entre 1 y 5
+
+var estecuadro = document.getElementById(cuadro);
+estecuadro.innerHTML="HOLA";
