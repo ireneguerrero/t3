c0a4a9776422ea713fed91c2c142d2c9ec137edb
ireneguerrero <irene2g222@gmail.com>
2023-04-18 20:25:08 +0200

    a

--- flex/flex2piramide.js
+++ flex/flex2piramide.js
@@ -1,7 +1,7 @@
 'use strict'; //nos ayuda a no dejar pasar ningún error
 
 //pedimos el número de cuadro donde escribir HOLA
-var cuadro = prompt("Diga qué cuadro quiere saludar (1 - 5)", "");
+var cuadro = prompt("Diga en qué cuadro quiere la pirámide (1 - 5)", "");
 cuadro = parseInt(cuadro);
 //cuadro ahora será una variable numérica, supuestamente entre 1 y 5
 
