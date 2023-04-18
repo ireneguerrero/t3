330f06d33779b7f18261927d46cbc61e34f291f9
ireneguerrero <irene2g222@gmail.com>
2023-04-18 19:44:35 +0200

    a

--- flex/flex2.js
+++ flex/flex2.js
@@ -5,5 +5,7 @@ var cuadro = prompt("Diga qué cuadro quiere saludar (1 - 5)", "");
 cuadro = parseInt(cuadro);
 //cuadro ahora será una variable numérica, supuestamente entre 1 y 5
 
-var estecuadro = document.getElementById(cuadro);
-estecuadro.innerHTML="HOLA";
+var selector = "c" + cuadro;
+
+var estecuadro = document.getElementById(selector);
+estecuadro.innerHTML = "HOLA";
